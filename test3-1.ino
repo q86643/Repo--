c48a6531a5a1b8f07e0b41816ff10eb8622f90a4
q86643 <q86643@gmail.com>
2017-04-17 19:22:30 +0800

    Add files via upload

---
+++ test3-1.ino
@@ -0,0 +1,40 @@
+#include "SevSeg.h"
+SevSeg sevseg;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("\ngroup : 13\n"); 
+  Serial.print("04050865\n"); 
+ Serial.println("03050095");
+ 
+  byte numDigits = 4;
+  byte digitPins[] = {5,4,3,2};
+  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+  sevseg.setBrightness(90);
+
+
+}
+void loop()
+{
+  int val;
+    if(Serial.available())
+    {
+      val = Serial.parseInt();
+      Serial.println(val);    
+      if(val>=0 && val<10000)
+      {
+        sevseg.setNumber(val);
+          sevseg.refreshDisplay();
+        }
+      else
+        Serial.println("range error");  
+      
+      
+    }
+    else
+            sevseg.refreshDisplay();
+
+    
+}
